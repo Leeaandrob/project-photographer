b35c0bfa8d5019f3fc5ebfc949ab7e86dbef6fce
Leandro Barbosa <leandrobar93@gmail.com>
2016-01-03 22:51:13 -0200

    update portifolio

--- photographer/portifolio/admin.py
+++ photographer/portifolio/admin.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 from django.contrib import admin
 
-from .models import (Category, Album, Photo)
+from .models import (Album, Photo)
 
-admin.site.register(Category)
 admin.site.register(Album)
 admin.site.register(Photo)
--- photographer/portifolio/models.py
+++ photographer/portifolio/models.py
@@ -3,16 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __unicode__(self):
-        return self.name
-
-
 class Album(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey('Category')
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField()
     client = models.CharField(max_length=255, null=True, blank=True)
