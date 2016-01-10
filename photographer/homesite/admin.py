c5ce5e9339df49a03ffb83d1a4b0051014ebc6f4
Leandro Barbosa <leandrobar93@gmail.com>
2016-01-10 15:11:21 -0200

    adding news admin

--- photographer/homesite/admin.py
+++ photographer/homesite/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import (Home, AboutMe)
+from .models import (Home, AboutMe, Contact)
 
 admin.site.register(Home)
 admin.site.register(AboutMe)
+admin.site.register(Contact)
