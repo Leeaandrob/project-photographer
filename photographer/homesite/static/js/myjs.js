a389c236405990797b8c6b77cb477b77cc0d8d39
Leandro Barbosa <leandrobar93@gmail.com>
2016-02-24 21:51:55 -0300

    fix problems

--- photographer/homesite/static/js/myjs.js
+++ photographer/homesite/static/js/myjs.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
-	var div = document.getElementById('nav-mobile'), nav = document.getElementsByClassName('nav-holder')[0];
+	var div = document.getElementById('nav-mobile'),
+		nav = document.getElementsByClassName('nav-holder')[0];
+
 	div.addEventListener('click', function (e){
-		nav.style.display = "block";
+		$('.nav-holder').toggle();
 	});
 });
